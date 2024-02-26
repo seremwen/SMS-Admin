c7cd2fe1e76340b3e8c3e5d488e4838985849eef
seremwen <seremwe.81@gmail.com>
2024-02-26 00:05:33 +0000

    Project Set up

---
+++ src/main/java/zw/nseremwe/SmsAdminApplication.java
@@ -0,0 +1,13 @@
+package zw.nseremwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsAdminApplication.class, args);
+    }
+
+}
