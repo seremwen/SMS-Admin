10e60a0eb56d8137bd3f8b7eb5c523fff6c8de8f
seremwen <seremwe.81@gmail.com>
2024-02-26 01:26:46 +0000

    eureka client configuration

--- src/main/java/zw/nseremwe/SmsAdminApplication.java
+++ src/main/java/zw/nseremwe/SmsAdminApplication.java
@@ -1,18 +1,35 @@
 package zw.nseremwe;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
-public class SmsAdminApplication {
+@RestController
+public class SmsAdminApplication implements GreetingController {
 
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
     public static void main(String[] args) {
         SpringApplication.run(SmsAdminApplication.class, args);
     }
-
+    @Override
+    public String greeting() {
+        return String.format(
+                "Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+    }
 }
